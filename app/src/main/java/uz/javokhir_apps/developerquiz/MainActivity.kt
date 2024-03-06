23f76320e9b14723ceafceb9f2d3b05ff705b219
Javokhir <achilovjavokhir@gmail.com>
2024-03-06 11:40:25 -0700

    Developer quiz app start

---
+++ app/src/main/java/uz/javokhir_apps/developerquiz/MainActivity.kt
@@ -0,0 +1,31 @@
+package uz.javokhir_apps.developerquiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import uz.javokhir_apps.developerquiz.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    lateinit var quizModelList: MutableList<QuizModel>
+    lateinit var adapter: QuizListAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        quizModelList= mutableListOf()
+        getDataFromFirebase()
+    }
+    private fun setupRecyclerView(){
+        adapter= QuizListAdapter(quizModelList)
+        binding.recyclerView.layoutManager= LinearLayoutManager(this)
+        binding.recyclerView.adapter=adapter
+    }
+    private fun getDataFromFirebase(){
+        quizModelList.add(QuizModel("1","Android","All the basic programming","15"))
+        setupRecyclerView()
+    }
+}
